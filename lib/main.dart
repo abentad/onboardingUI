f135ccdc490d29e327768c401b98b9b6a283cb49
Abenezer <abentad4@gmail.com>
2020-10-22 22:23:43 +0300

    after shared prefs working

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:onboard_ui/main_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'home_screen.dart';
 
@@ -10,7 +14,46 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      home: LoaderPage(),
+    );
+  }
+}
+
+class LoaderPage extends StatefulWidget {
+  @override
+  _LoaderPageState createState() => _LoaderPageState();
+}
+
+class _LoaderPageState extends State<LoaderPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(new Duration(milliseconds: 200), () {
+      checkFirstSeen();
+    });
+  }
+
+  Future checkFirstSeen() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool _seen = (prefs.getBool('seen') ?? false);
+
+    if (_seen) {
+      Navigator.of(context).pushReplacement(
+          new MaterialPageRoute(builder: (context) => MainPage()));
+    } else {
+      prefs.setBool('seen', true);
+      Navigator.of(context).pushReplacement(
+          new MaterialPageRoute(builder: (context) => HomePage()));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text("loading"),
+      ),
     );
   }
 }
